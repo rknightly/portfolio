fcf74188b2ac6727a0245506f39215950cbc6e75
Ryan Knightly <reknightly@gmail.com>
2020-07-03 03:41:38 -0500

    Update contact info and favicon

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,14 +14,12 @@ const Contact = () => {
         <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
-            </p>
+            <p className="contact-wrapper__text">{cta || 'Would you like reach out? Awesome!'}</p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              href={email ? `mailto:${email}` : 'https://www.linkedin.com/in/ryaneknightly/'}
             >
               {btn || "Let's Talk"}
             </a>
